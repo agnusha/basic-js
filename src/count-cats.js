c87439645a77191365604af8feba59f915ca1324
Agnia <agnia2000@inbox.ru>
2020-03-08 21:59:41 +0300

    init: start task BasicJS. Finish Count cats! and Carbon dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  matrix.forEach(function(item) {
+    count+=item.filter(x=> x=="^^").length;
+  });
+  return count;
 };
